0777c2018ea6796064c6226cfcc3a067b6238ea1
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-04-17 05:35:49 +0100

    update

---
+++ conversion.py/convert.py
@@ -0,0 +1,15 @@
+salary = 4800
+vacation_days = 23
+if salary > 5000 and vacation_days > 40:
+    print("I will take this job.")
+else:
+    print("This job sucks! Give it to jerry.")
+
+    money_text= input("amount:")
+    money = float(money_text)
+    print(money)
+
+    x = 2
+    y = 3
+    z = (x + 2) * y
+    print(z + y)
---
+++ herot.py
@@ -0,0 +1,12 @@
+print("Welcome to my Github repo")
+name = input("Tell me your name: ")
+print(f"Hi {name}!")
+age_text = input("How old are you?; ")
+age = int(age_text)
+
+a = 5
+b = 2
+x = a + b
+y = a % b
+z = a / b
+print(x, y, z)
