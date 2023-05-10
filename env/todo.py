804a81186cce2272e5b19b0e15fb97175f8c4d47
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-10 20:16:22 +0100

    queue

---
+++ env/todo.py
@@ -0,0 +1,9 @@
+from collections import deque
+queue = deque(["Eric", "John", "Micheal"])
+queue.append("Terry")
+queue.append("Tosinkuzzy")
+queue.append("Nextking")
+queue.popleft()
+queue.popleft()
+queue.popleft()
+queue
