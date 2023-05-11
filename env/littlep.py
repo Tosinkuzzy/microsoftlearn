b6db5960fdf44435296c48ebf94964fa587fdc34
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-11 20:07:50 +0100

    List project

---
+++ env/littlep.py
@@ -0,0 +1,17 @@
+# Build a sentence 
+# Ask user for input
+names = input("What is your name? ")
+# Accessing Element in a List
+print(names.title())
+# Using individual Values from List
+message = "Welcome to royal luxury Car!"
+print(message)
+# More sentence
+car = ['tesla', 'toyota', 'honda', 'innoson']
+# Adding
+car.append('ford')
+# Accessing sentence
+print(car[4].title())
+# message 
+message = "I would like to own a " + car[4].title() + "."
+print(message)
