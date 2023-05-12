2af26fa9f87a532c4f409c664057a73c7d4d1dba
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-12 04:52:43 +0100

    innosonorder

--- env/littlep.py
+++ env/littlep.py
@@ -4,23 +4,23 @@ names = input("What is your name? ")
 # Accessing Element in a List
 print("Hi! " + names.title())
 # Using individual Values from List
-message = "Welcome to royal luxury Car!"
+message = "Welcome to Innoson Vehicles!"
 print(message)
 # More sentence
 # Sorting a List Permanently with the sort() Method
-car = ['bmw', 'innoson', 'audi', 'toyota', 'subaru', 'tesla', 'toyota', 'honda', 'innoson']
+car = ['fox', 'umu', 'carrier', 'ivm carrier', 'g4', 'g6', 'g40', 'ivm carrier 4x4 pickup']
 # Changing element
-car[1] = 'elegant'
-car[3] = 'kia'
+car[1] = 'carrier 4x4'
+car[3] = 'g5'
 # Adding element
-car.append('ford')
+car.append('g7')
 # Change the order of the list
 car.sort()
 # Reverse order
 car.sort(reverse=True)
 print(car)
 # Getting input from user
-car = input("Favourite car: ")
+car = input("Your Order? ")
 # message 
 message = "I would like to own a " + car.title() + "."
 print(message)
