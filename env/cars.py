f892a97b4e175d814b3d4957fbfed8ab71fa7332
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-12 03:58:53 +0100

    update

---
+++ env/cars.py
@@ -0,0 +1,5 @@
+# Sorting a List Permanently with the sort() Method
+cars = ['bmw', 'innoson', 'audi', 'toyota', 'subaru']
+# Changing the order of the list
+cars.sort()
+print(cars)
--- env/littlep.py
+++ env/littlep.py
@@ -7,15 +7,18 @@ print("Hi! " + names.title())
 message = "Welcome to royal luxury Car!"
 print(message)
 # More sentence
-car = ['tesla', 'toyota', 'honda', 'innoson']
+# Sorting a List Permanently with the sort() Method
+car = ['bmw', 'innoson', 'audi', 'toyota', 'subaru', 'tesla', 'toyota', 'honda', 'innoson']
 # Changing element
 car[1] = 'elegant'
-print(car)
+car[3] = 'kia'
 # Adding element
 car.append('ford')
+# Change the order of the list
+car.sort()
 print(car)
 # Getting input from user
 car = input("What car do you want? ")
 # message 
-message = "I would like to own a " + car + "."
+message = "I would like to own a " + car.title() + "."
 print(message)
