a8b7dc7a3737b6667d401221440b91d1f68bb3d9
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-13 03:48:27 +0100

    color

---
+++ env/color.py
@@ -0,0 +1,14 @@
+def complementary_color(color):
+    if color == "red":
+        print("Cyan")
+    elif color == "yellow":
+        print("White")
+    elif color == "black":
+        print("Green")
+    else:
+        print("Unknown")
+
+complementary_color("red")
+complementary_color("yellow")
+complementary_color("black")
+complementary_color("green")
--- env/speed_ticket.py
+++ env/speed_ticket.py
@@ -5,3 +5,4 @@ def get_ticket(speed):
         return "Speeding"
     else:
         return "Safe"
+    
