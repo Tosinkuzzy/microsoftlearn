3526c8f1673e5e764636b547b460da0dcebce3ba
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-05-29 00:51:02 +0100

    update

--- env/favourite_languages.py
+++ env/favourite_languages.py
@@ -1,36 +1,12 @@
-# Name associated in a list.
-favourite_languages = {
-    'jen': ['python', 'ruby'],
-    'sarah': ['c'],
-    'edward': ['ruby', 'vb.net'],
-    'phil': ['python', 'java'],
-   'adam': ['pascal', 'javascript'],
-}
-# Looping through Dictionary
-# Using variable name to hold each values in Dictionary
-for name, languages in favourite_languages.items():
-    print("\n " + name.title() + "'s favourite languages are:")
-    # Loop to run each person's list of favourite languages.
-    for language in languages:
-        print("\t" + language.title())
-# Looping through dictionary
-# for name, language in favourite_languages.items():
-  #  print(name.title() + "'s favourite language is " +
-   #       language.title() + ".")
+from collections import OrderedDict
 
-#for values in favourite_languages.values():
-   # print(values.title())
+favourite_languages = OrderedDict()
 
-# friends = ['phil', 'edward']
-# Looping through a dict in order
-# for name in sorted(favourite_languages.keys()):
-# for name in sorted(favourite_languages.keys()):
-    #print(name.title() + ", thank you for taking the poll.")
+favourite_languages['jen'] = 'python'
+favourite_languages['sarah'] = 'c'
+favourite_languages['edward'] = 'ruby'
+favourite_languages['phil'] = 'python'
 
-# if name in friends:
-    #print("  Hi " + name.title() + ", I see your favourite language is " +
-               #favourite_languages[name].title() + "!")
-    
-# if 'erin' not in favourite_languages.keys():
-    #print("Erin, please take our poll")
-    
+for name, language in favourite_languages.items():
+    print(name.title() + "'s favourite language is " + 
+          language.title() + ".")
