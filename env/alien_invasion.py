03ce84dee58ae9804f79626ac18177f4cc8b8d17
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-06-01 16:17:07 +0100

    updates

---
+++ env/alien_invasion.py
@@ -0,0 +1,28 @@
+import pygame
+
+from settings import Settings
+
+def run_game():
+    # Initialize pygame, settings, and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    pygame.display.set_caption("Alien Invasion")
+    # Set the background color.
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keyboard and mouse events.
+        
+
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
---
+++ env/ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship():
+
+    def __init__(self, screen):
+        """Initialize the ship and set it's starting position"""
+        self.screen = screen
+
+    # Load the ship image and get it's rest.
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+    self.screen_rect = screen.get_rect()
+
+    # Start each new ship at the bottom center of the screen.
+    self.rect.centerx = self.screen_rect.centrex
+    self.rect.bottom = self.screen_rect.bottom
+
+def blitme(self):
+    """"Draw the ship at it's current location"""
+    self.screen.blitme(self.image, self.rect)
