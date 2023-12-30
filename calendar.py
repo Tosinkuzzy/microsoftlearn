fd381b3b96496d7760eb5a1aabf0b06c34d685a1
Tosin Ayomide Adeleye <Adeleye608@gmail.com>
2023-12-30 22:39:50 +0100

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,4 +1,5 @@
-// The code will print the date for August 4th, 2018.
-from calendar import*
+""" The code will print the date for August 4th, 2018. """
+
+from calendar import *
 year = int(input('Enter Year:'))
 print(calendar(year, 2, 1, 8, 4))
